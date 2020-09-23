09567e66f2303c7668074860a3e4fa76b338a7da
PragadeeshKathirvelu <pragakathir@gmail.com>
2020-09-23 21:21:35 +0530

    Farm
    farm has been added to the repository.

---
+++ Farm/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+
+
+    private void LateUpdate()
+    {
+
+
+    }
+}
---
+++ Farm/Assets/Scripts/Tractor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tractor : MonoBehaviour
+{
+    public float tractspeed;
+    public float turnspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * tractspeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * tractspeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down, turnspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, turnspeed * Time.deltaTime);
+        }
+    }
+}
